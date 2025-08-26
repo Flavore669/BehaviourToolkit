a4a0396b9e14cc413260617d1687652d03f313ea
Flavore669 <dirkosh2@gmail.com>
2025-08-25 22:23:26 -0500

    base implementation done

--- examples/fsm_character_controller/MrBlacksmith.gd
+++ examples/fsm_character_controller/MrBlacksmith.gd
@@ -6,10 +6,10 @@ const SPRINT_MULTIPLIER := 1.7
 
 
 var movement_direction := Vector2.ZERO
-
+var in_action := false
 
 @onready var state_machine := $FSMController
-@onready var sprite := $Character
+@onready var sprite := $Sprite2D
 @onready var animation_player := $AnimationPlayer
 @onready var particles_walking := $ParticlesWalking
 
@@ -20,13 +20,5 @@ func _physics_process(_delta):
 		Input.get_axis("ui_up", "ui_down")
 	)
 
-
 func _ready():
 	state_machine.start()
-
-func _on_fsm_controller_state_changed(state: FSMState) -> void:
-	print(state.name)
-
-
-func _on_movement_fsm_nested_state_changed(state: FSMState) -> void:
-	print(state.name)
