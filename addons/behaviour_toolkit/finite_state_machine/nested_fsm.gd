05aa27ca857a5ecd29e8df2eddedae45e6566f64
Flavore669 <dirkosh2@gmail.com>
2025-09-05 00:03:13 -0500

    added comments to NestedFSM, next is adjusting configuration warnings in states

--- addons/behaviour_toolkit/finite_state_machine/nested_fsm.gd
+++ addons/behaviour_toolkit/finite_state_machine/nested_fsm.gd
@@ -2,6 +2,14 @@
 extends FSMState
 class_name NestedFSM
 
+## An implementation of a simple finite state machine.
+##
+## The Nested Finite State Machine is a state that contains a FiniteStateMachine insde
+## This Instanced FiniteStateMachine inherits values from the parent FiniteStateMachine.
+## On ready, the NestedFSM will reparent each child that is an FSMState or NestedFSM to the new FiniteStateMachine.
+## To implement your logic you can override the [code]_on_enter, _on_update and
+## _on_exit[/code] methods when extending the node's script.
+
 
 ## The signal emitted when the fsm's state changes.
 signal nested_state_changed(state: FSMState)
@@ -9,6 +17,7 @@ signal nested_state_changed(state: FSMState)
 @export var initial_state : FSMState
 @export var verbose : bool
 
+# Create a child FSM instance to manage the nested states
 @onready var fsm = FiniteStateMachine.new()
 
 func _ready() -> void:
@@ -17,27 +26,38 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 	
-	
+	# Find all direct child nodes that are FSMStates
 	var nested_states : Array[Node] = get_children().filter(func(n): return n is FSMState)
+	# Find all direct child nodes that are NestedFSMs (for hierarchical nesting)
 	var nested_fsms : Array[Node] = get_children().filter(func(n): return n is NestedFSM)
 	
+	# Add the FSM container as a child node, and rename it for clarity in scene tree
 	add_child(fsm)
 	fsm.name = "NestedFSM"
 	
+	# Move all FSMState children to the internal FSM container
 	for state in nested_states: 
 		state.reparent(fsm)
 		
+	# Move all nested FSMs to the internal FSM container
 	for nested_fsm in nested_fsms: 
 		nested_fsm.reparent(fsm)
 		
-	fsm.initial_state = initial_state
-	fsm.verbose = verbose
+	# Configure the internal FSM
+	fsm.initial_state = initial_state  # Set the starting state
+	fsm.verbose = verbose              # Set debug output preference
 	
+	# Get reference to the parent state machine
+	# After reparenting, our parent should always be a FiniteStateMachine
+	# (either the root FSM or another NestedFSM's internal FSM)
 	var parent_state_machine : FiniteStateMachine = get_parent()
-	fsm.actor = parent_state_machine.actor
-	fsm.process_type = parent_state_machine.process_type
-	fsm.blackboard = parent_state_machine.blackboard
+		 
+	# Share resources with the parent FSM
+	fsm.actor = parent_state_machine.actor           # Share the actor reference
+	fsm.process_type = parent_state_machine.process_type  # Share process type (physics/idle)
+	fsm.blackboard = parent_state_machine.blackboard     # Share the blackboard (shared data)
 	
+	# Connect the internal FSM's state_changed signal to our nested_state_changed signal
 	fsm.state_changed.connect(func(state: FSMState): nested_state_changed.emit(state))
 
 # Executes after the state is entered.
@@ -47,6 +67,10 @@ func _on_enter(_actor: Node, _blackboard: Blackboard) -> void:
 # Executes before the state is exited.
 func _on_exit(_actor: Node, _blackboard: Blackboard) -> void:
 	fsm.active = false
+
+
+# TODO: Improve configuration warnings for Nested FSM
+# Now that NestedFSMs exist, there should be one to account for when the user selects a state from another FSM
 
 
 # Add custom configuration warnings
